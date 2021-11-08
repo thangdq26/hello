6faab48d9c2e4cf2c87713af0b5a5b5c7a216d35
thangdq26 <thangdq@mirabo.com.vn>
2021-11-08 14:38:09 +0700

    commit develop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,6 @@
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
